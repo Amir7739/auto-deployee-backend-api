1b621e02172db0ab9c461fa3364065af8fb8d52f
Amir7739 <mdamira949@gmail.com>
2024-10-20 15:13:40 +0530

    add testing products routes

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.use(express.json())
 
 app.use('/api/users',userRoutes)
 
+app.use('/api/products', (req, res) => {
+    return res.status(200).json({
+        message: "This is new feature"
+    })
+})
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`))
